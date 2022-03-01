e68252eb91ff38acdc2fb567a4282538aa2f6e2c
pabloplr <pablopezromero@gmail.com>
2022-03-02 00:28:06 +0100

    arreeglo textos

--- router/rutas.js
+++ router/rutas.js
@@ -2,9 +2,10 @@ const express = require('express') //Requerimos Express
 const router = express.Router();
 const sesion = require('../public/js/sesion.js');
 
+//al entrar en raiz
 router.get('/', (req, res) => {
     if(sesion.isLoged()){
-        res.render("index", { titulo: "mi titulo dinámico" })
+        res.render("index", { titulo: "NBA" })
     }else{
         res.redirect('/login')
     } 
